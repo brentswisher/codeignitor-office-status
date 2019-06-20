3d951e0746804f0d21ace1f2528e6367c7762998
Brent Swisher <brent@brentswisher.com>
2019-06-20 15:47:05 -0400

    Add customized bootstrap styles

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,15 @@
-  <em>&copy; 2019</em>
-        </body>
+				<footer>
+					<p>
+						&copy;<?= date('Y') ?> <a href="http://www.gvsu.edu/">Grand Valley State University</a> - <a href="http://www.gvsu.edu/webteam/">GVSU Web Team</a>
+					</p>
+				</footer>
+			</div>
+		</div>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<cf_jquery>
+
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	</body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,82 @@
-<html>
-        <head>
-                <title>CodeIgniter Tutorial</title>
-        </head>
-        <body>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title><?=$title ?></title>
+			
+		<!-- Bootstrap -->
+		<link rel="stylesheet" type="text/css" href="<? echo base_url();?>assets/css/bootstrap.custom.min.css" />
 
-                <h1><?php echo $title; ?></h1>
+		<style>
+			.navbar {
+				border-radius: 0;
+			}
+			.req {
+				color: #F00;
+			}
+			.container {
+				width: 100%;
+			}
+			.table-striped th:last-child, .table-striped td:last-child {
+				width: 1px;
+				white-space: nowrap;
+			}
+		</style>
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<meta http-equiv="refresh" content="300">
+	</head>
+	<body>
+		<div class="navbar navbar-inverse" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<!-- <cfIf sb.isLoggedIn> -->
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					<!-- </cfIf> -->
+					<a class="navbar-brand" href="index.htm">
+						<?=$title ?>
+					</a>
+				</div>
+				<!-- <cfIf sb.isLoggedIn> -->
+					<div class="collapse navbar-collapse">
+						<ul class="nav navbar-nav navbar-right">
+							<li>
+								<a href="#">Welcome, <!-- <cfOutput>#session.user.fName# #session.user.lName#</cfOutput> --></a>
+							</li>
+							<li>
+								<a href="/logout"><span class="glyphicon glyphicon-lock"></span> logout</a>
+							</li>
+						</ul>
+						<ul class="nav navbar-nav">
+							<!-- <li <cfIf sb.routing[1] EQ "status">class="active"</cfIf>> -->
+							<li>
+								<a href="/">Dashboard</a>
+							</li>
+							<!-- <li <cfIf sb.routing[1] EQ "employee">class="active"</cfIf>> -->
+							<li>
+								<a href="/users">Employees </a>
+							</li>
+							<!-- <li <cfIf sb.routing[1] EQ "note">class="active"</cfIf>> -->
+							<li>
+								<a href="/status">Status </a>
+							</li>
+						</ul>
+					</div>
+				<!-- </cfIf> -->
+			</div>
+		</div>
+		<div class="container">
+			<div class="content">
