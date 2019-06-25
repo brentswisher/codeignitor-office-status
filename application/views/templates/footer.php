1fbbabf03b766202d210ca1a4214b7c6284cabc0
Brent Swisher <swisherb@gvsu.edu>
2019-06-25 17:00:08 -0400

    Add jQuery via cdn instead of custom tag

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -6,10 +6,9 @@
 			</div>
 		</div>
 
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<cf_jquery>
-
+		
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 	</body>
 </html>
